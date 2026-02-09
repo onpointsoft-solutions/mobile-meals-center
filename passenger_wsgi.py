a534684840d8dcb0d0a3075c81b72efa980f0691
OnpointSoftwares <vincentbettoh@gmail.com>
2026-02-09 15:41:07 +0300

    changes

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,11 +1,4 @@
 import os
 import sys
 
-# 🔧 settings module (VERY IMPORTANT)
-os.environ.setdefault(
-    "DJANGO_SETTINGS_MODULE",
-    "config.settings"
-)
-
-# 🔧 load django wsgi application
 from config.wsgi import application
