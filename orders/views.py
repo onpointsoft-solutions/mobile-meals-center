ca612fb3d21f299311efd4d6d22299d1ac57a495
OnpointSoftwares <vincentbettoh@gmail.com>
2026-02-09 16:08:16 +0300

    changes

--- orders/views.py
+++ orders/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.views.generic import ListView, DetailView, CreateView, TemplateView, View
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.urls import reverse_lazy
 from django.contrib import messages
 from django.db import transaction
