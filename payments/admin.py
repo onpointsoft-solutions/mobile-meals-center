9810d9e2b6a83e1b52fa92de79a3696399867329
OnpointSoftwares <vincentbettoh@gmail.com>
2026-02-15 19:56:03 +0300

    changes

--- payments/admin.py
+++ payments/admin.py
@@ -6,8 +6,8 @@ from .models import Payment
 class PaymentAdmin(admin.ModelAdmin):
     list_display = ('payment_id', 'order', 'user', 'amount', 'status', 'payment_method', 'created_at')
     list_filter = ('status', 'payment_method', 'created_at')
-    search_fields = ('id', 'order__id', 'user__username', 'stripe_payment_intent_id')
-    readonly_fields = ('id', 'stripe_payment_intent_id', 'stripe_charge_id', 'created_at', 'updated_at', 'paid_at')
+    search_fields = ('id', 'order__id', 'user__username', 'paystack_reference')
+    readonly_fields = ('id', 'paystack_reference', 'paystack_transaction_id', 'created_at', 'updated_at', 'paid_at')
     
     def payment_id(self, obj):
         return obj.payment_id
